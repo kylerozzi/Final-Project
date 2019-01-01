c300a7d85883f815740261d64c8450cb8c5155a8
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2019-01-01 18:26:54 -0500

    Updated from Brython Server: 1/1/2019 6:26:54 PM

--- finalproj.py
+++ finalproj.py
@@ -1,3 +1,5 @@
 #Kyle Rozzi
 
 print("Hi there! Welcome to Adventure of the Cave! Make decisions to alter the game and end victorious!")
+
+q1 = input("You are taking your regular walk in the forest. You look to your right and catch sight of a cave. Do you enter? Enter “Y” for yes and “N” for no: ")
