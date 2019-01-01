a309138f5e20c63b7f22a74e9f3abb74ed1b4f2a
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2019-01-01 18:34:18 -0500

    Updated from Brython Server: 1/1/2019 6:34:18 PM

--- finalproj.py
+++ finalproj.py
@@ -3,3 +3,21 @@
 print("Hi there! Welcome to Adventure of the Cave! Make decisions to alter the game and end victorious!")
 
 q1 = input("You are taking your regular walk in the forest. You look to your right and catch sight of a cave. Do you enter? Enter “Y” for yes and “N” for no: ")
+
+if q1 == "N":
+    print("You lack a sense of adventure: GAME OVER")
+elif q1 == "Y":
+    q2 = input("You enter the cave, and you see a lantern on the ground.Do you pick it up?  Enter “Y” for yes and “N” for no: ")
+    if q2 == "N":
+        print("You’re in a dark cave and you don’t pick up a lantern - smart move: GAME OVER")
+    elif q2 == "Y":
+        q3 = input("You pickup the lantern and continue walking further into the cave. At some point, you catch sight of a sword and a gold brick sitting next to one another on the ground. You can only take one because of the lantern. Enter “S” for sword and “G” for the gold brick: ")
+        if q3 == "G":
+            print("Although it’s heavy you are able to proceed further into the cave. At some point, you catch sight of a door being guarded by sleeping wolf. You can either decide to turn around and go back or quietly go past the dog through the door: Enter “T” for turn around and “Q” for quietly going past: ")
+            
+    else:
+        pass
+
+else:
+    pass
+    
