ff290c22a4fc32a2a632edc666be0bef4a281dca
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2019-01-01 19:02:31 -0500

    Updated from Brython Server: 1/1/2019 7:02:31 PM

--- finalproj.py
+++ finalproj.py
@@ -2,7 +2,7 @@
 
 print("Hi there! Welcome to Adventure of the Cave! Make decisions to alter the game and end victorious!")
 
-q1 = input("You are taking your regular walk in the forest. You look to your right and catch sight of a cave. Do you enter? Enter “Y” for yes and “N” for no: ")
+q1 = input("You are walking through the forest and catch sight of a cave. Do you enter? Enter “Y” for yes and “N” for no: ")
 
 if q1 == "N":
     print("You lack a sense of adventure: GAME OVER")
@@ -17,7 +17,7 @@ elif q1 == "Y":
             if q4 == "T":
                 print("You are a coward: GAME OVER")
             elif q4 == "Q":
-                q5 == input("You quietly sneak through the door without waking the wolf, but once you enter the new section of the cave, you are confronted by a massive figure. The figure bellows “Turn around or Die!” You can either attempt to bribe them with the gold brick or club them over the head with it: Enter “B” to bribe and “C” to club: ")
+                q5 = input("You quietly sneak through the door without waking the wolf, but once you enter the new section of the cave, you are confronted by a massive figure. The figure bellows “Turn around or Die!” You can either attempt to bribe them with the gold brick or club them over the head with it: Enter “B” to bribe and “C” to club: ")
                 if q5 == "B":
                     print("You attempt to bribe the figure, and they respond with “Ha-Ha, foolish traveler. I only accepts bribes of swords. Not worthless gold bricks.” The figure proceeds to kill you: Game Over")
                 elif q5 == "C":
@@ -56,14 +56,14 @@ elif q1 == "Y":
                 elif q10 == "B":
                     q11 = input("You attempt to bribe the figure with the sword and it works! You are free to continue through the cave. After a while, you reach the end of the cave. There are two rooms that you can enter. One of them is filled with treasure and the other has a puppy. Which do you choose: Enter “T” for the treasure room and “P” for the puppy room: ")
                     if q11 == "P":
-                        q12 = input("You enter the room. You have the choice to take a puppy home and care for it as your own:  Enter “Y” to take the puppy and “N” to leave empty handed: ")
+                        q12 = input("You enter the room. Enter “Y” to take the puppy home and “N” to leave empty handed: ")
                         if q12 == "N":
                             print("You want to end your adventure empty handed? Well, it's your prerogative - Thank you for playing: The End")
                         elif q12 == "Y":
                             print("Fantastic decision. You take home the puppy and end up with a great companion. A successful adventure, I’d say: The End")
 
                     elif q11 == "T":
-                        q13 = input("You enter the room. You have the choice to take as much treasure as you can or head home empty handed:  Enter “Y” to take the treasure and “N” to leave empty handed: ")
+                        q13 = input("You enter the room. Enter “Y” to take the treasure and “N” to leave empty handed: ")
                         if q13 == "N":
                             print("You want to end your adventure empty handed? Well, it's your prerogative - Thank you for playing: The End")
                         elif q13 == "Y":
